e87bd2719aa9ad36f98351a5a93e799066d634ff
Alexander <adimitrov23@gmail.com>
2019-12-03 16:39:24 +0200

    removed old unused code

--- src/main.cpp
+++ src/main.cpp
@@ -2,14 +2,6 @@
 #include <fstream>
 #include "sudoku.hpp"
 
-const int SUDOKU_SIZE = 9;
-void readSudoku(char sud[SUDOKU_SIZE][SUDOKU_SIZE], std::istream& in)
-{
-    for (int i = 0; i < SUDOKU_SIZE; ++i)
-        for (int j = 0; j < SUDOKU_SIZE; ++j)
-            in >> sud[i][j];
-}
-
 int main()
 {
     Sudoku sud;
