13de5b54efd62267f7fd56d6abfeb5e759d5a936
Alexander <adimitrov23@gmail.com>
2019-12-03 16:26:01 +0200

    updated simple solve algorithm and print method

--- src/sudoku.cpp
+++ src/sudoku.cpp
@@ -13,9 +13,17 @@ Sudoku::Sudoku() {
 
 void Sudoku::print(std::ostream& out) const {
     for (int i = 0; i < SIZE; ++i) {
-        for (int j = 0; j < SIZE; ++j)
+        for (int j = 0; j < SIZE; ++j) {
             out << matrix[i][j] << ' ';
+            if (j % SMALL_SIZE == SMALL_SIZE - 1 && j < SIZE - 1)
+                out << "# ";
+        }
         out << std::endl;
+        if (i % SMALL_SIZE == SMALL_SIZE - 1 && i < SIZE - 1) {
+            for (int j = 0; j < 2*SIZE + 3; ++j)
+                out << "#";
+            out << std::endl;
+        }
     }
 }
 
@@ -24,14 +32,67 @@ void Sudoku::read(std::istream& in) {
     for (int i = 0; i < SIZE; ++i) {
         for (int j = 0; j < SIZE; ++j) {
             in >> matrix[i][j];
+        }
+    }
+}
+
+
+bool Sudoku::isSolved() const {
+    for (int i = 0; i < SIZE; ++i)
+        for (int j = 0; j < SIZE; ++j)
+            if (matrix[i][j] == '0')
+                return false;
+    return true;
+}
+
+
+void Sudoku::solve() {
+    simpleSolve();
+    if (!isSolved())
+        complexSolve();
+}
+
+
+void Sudoku::simpleSolve() {
+    removeGuesses();
+}
+
+
+void Sudoku::complexSolve() {
+    // TODO: Implement a simple backtracking algorithm.
+}
+
+
+void Sudoku::removeGuesses() {
+    int counter = 0;
+    for (int i = 0; i < SIZE; i++) {
+        for (int j = 0; j < SIZE; j++) {
             if (matrix[i][j] != '0') {
                 guesses[i][j].clear();
                 for (int k = 0; k < SIZE; k++) {
-                    guesses[k][j].erase(matrix[i][j]);
-                    guesses[i][k].erase(matrix[i][j]);
-                    guesses[(i / SMALL_SIZE) * SMALL_SIZE + k / SMALL_SIZE][(j / SMALL_SIZE) * SMALL_SIZE + k % SMALL_SIZE].erase(matrix[i][j]);
+                    counter += guesses[k][j].erase(matrix[i][j]);
+                    counter += guesses[i][k].erase(matrix[i][j]);
+                    counter += guesses[(i / SMALL_SIZE) * SMALL_SIZE + k / SMALL_SIZE][(j / SMALL_SIZE) * SMALL_SIZE + k % SMALL_SIZE].erase(matrix[i][j]);
                 }
             }
         }
     }
+    if (counter > 0)
+        setSingleGuesses();
+}
+
+
+void Sudoku::setSingleGuesses() {
+    int counter = 0;
+    for (int i = 0; i < SIZE; i++) {
+        for (int j = 0; j < SIZE; j++) {
+            if (guesses[i][j].size() == 1) {
+                ++counter;
+                matrix[i][j] = *guesses[i][j].begin();
+                guesses[i][j].clear();
+            }
+        }
+    }
+    if (counter > 0)
+        removeGuesses();
 }
--- src/sudoku.hpp
+++ src/sudoku.hpp
@@ -12,11 +12,23 @@ public:
     Sudoku& operator=(const Sudoku&) = default;
     ~Sudoku() = default;
 
-    //char getAtPos(int row, int col) const;
-    //char& setAtPos(int row, int col);
-
+    // Prints the Sudoku in a formatted way using a stream
     void print(std::ostream& = std::cout) const;
+
+    // Reads the Sudoku using a stream
     void read(std::istream& = std::cin);
+
+    // Quadraticly checks if all boxes are set
+    bool isSolved() const;
+
+    // Solves the Sudoku
+    void solve();
+
+private:
+    void simpleSolve();
+    void complexSolve();
+    void removeGuesses();
+    void setSingleGuesses();
 
 private:
     const static int SIZE = 9;
