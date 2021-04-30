af1bf024e6af3f5a02b464b0e684a215809d5d94
dimbata23 <adimitrov23@gmail.com>
2021-04-30 19:42:30 +0300

    optimized and formatted

--- src/main.cpp
+++ src/main.cpp
@@ -1,24 +1,91 @@
+#include <filesystem>
 #include <iostream>
 #include <fstream>
 #include <chrono>
 #include "sudoku.hpp"
 
+using std::cout;
+
+void solveSingle( const char* path, bool showSolution = true, bool showExecTime = true );
+void testAllFrom( const char* dir, long long times, bool showIndividial = false );
+
+
 int main()
 {
+    solveSingle( "sudoku_samples/s16.txt" );
+    cout << "---------------------------------------------------------------------\n";
+    testAllFrom( "sudoku_samples", 1, true );
+    cout << "---------------------------------------------------------------------\n";
+    testAllFrom( "sudoku_samples", 10, false );
+
+    return 0;
+}
+
+
+using std::ifstream;
+using std::distance;
+
+using dir_it        = std::filesystem::directory_iterator;
+using hr_clock      = std::chrono::high_resolution_clock;
+using ms            = std::chrono::milliseconds;
+
+#define dur_cast_ms( arg )  std::chrono::duration_cast< std::chrono::milliseconds >( arg )
+
+
+void solveSingle( const char* path, bool showSolution, bool showExecTime )
+{
     Sudoku sud;
-    std::ifstream in("sudoku_samples/s16.txt");
+    ifstream in( path );
+    if ( !in )
+    {
+        cout << "Couldn't open file \"" << path << "\" for reading!\n";
+        return;
+    }
+
     in >> sud;
     in.close();
 
-    auto start = std::chrono::high_resolution_clock::now();
-    if (sud.solve()) {
-        auto end = std::chrono::high_resolution_clock::now();
-        auto duration = std::chrono::duration_cast<std::chrono::milliseconds>(end - start).count();
-        std::cout << "Solution found in " << duration << "ms." << std::endl;
-        sud.print();
-    } else {
-        std::cout << "Couldn't solve the Sudoku!" << std::endl;;
+    auto start = hr_clock::now();
+    if ( sud.solve() )
+    {
+        if ( showExecTime )
+        {
+            auto end        = hr_clock::now();
+            auto duration   = dur_cast_ms( end - start ).count();
+            cout << path << "'s solution found in " << duration << "ms.\n";
+        }
+
+        if ( showSolution )
+            sud.print();
+    }
+    else
+    {
+        cout << "Couldn't solve the Sudoku!\n";
+    }
+}
+
+
+void testAllFrom( const char* dir, long long times, bool showIndividial )
+{
+    auto        dirIt       = dir_it( dir );
+    long long   sudCount    = distance( dir_it( dir ), dir_it() );
+    long long   average     = 0;
+
+    for ( long long i = 0; i < times; i++ )
+    {
+        cout << "Solving " << sudCount << " sudokus... (" << i << " out of " << times << " times)\n";
+        auto    start       = hr_clock::now();
+
+        for ( const auto& entry : dirIt )
+            solveSingle( entry.path().generic_string().c_str(), false, showIndividial );
+        
+        auto    end         = hr_clock::now();
+        auto    duration    = dur_cast_ms( end - start ).count();
+        average            += duration;
+
+        cout << "Solved all " << sudCount << " sudokus in " << duration << " ms.\n";
     }
 
-    return 0;
+    average /= times;
+    cout << "Solved all " << sudCount << " sudokus in " << average << "ms. on average\n";
 }
--- src/sudoku.cpp
+++ src/sudoku.cpp
@@ -1,27 +1,34 @@
 #include "sudoku.hpp"
 
 
-Sudoku::Sudoku() {
-    for (int i = 0; i < SIZE; ++i) {
-        for (int j = 0; j < SIZE; ++j) {
-            matrix[i][j] = '0';
-            for (char c = '1'; c <= '9'; ++c)
-                guesses[i][j].insert(c);
+Sudoku::Sudoku()
+{
+    for ( int i = 0; i < SIZE; ++i )
+    {
+        for ( int j = 0; j < SIZE; ++j )
+        {
+            matrix[ i ][ j ] = '0';
+            for ( char ch = '1'; ch <= '9'; ++ch )
+                guesses[ i ][ j ].insert( ch );
         }
     }
 }
 
 
-void Sudoku::print(std::ostream& out, bool formatted) const {
-    for (int i = 0; i < SIZE; ++i) {
-        for (int j = 0; j < SIZE; ++j) {
-            out << matrix[i][j] << ' ';
-            if (formatted && j % SMALL_SIZE == SMALL_SIZE - 1 && j < SIZE - 1)
+void Sudoku::print( std::ostream& out, bool formatted ) const
+{
+    for ( int i = 0; i < SIZE; ++i )
+    {
+        for ( int j = 0; j < SIZE; ++j )
+        {
+            out << matrix[ i ][ j ] << ' ';
+            if ( formatted && j % SMALL_SIZE == SMALL_SIZE - 1 && j < SIZE - 1 )
                 out << "  ";
         }
         out << std::endl;
-        if (formatted && i % SMALL_SIZE == SMALL_SIZE - 1 && i < SIZE - 1) {
-            for (int j = 0; j < 2*SIZE + 3; ++j)
+        if ( formatted && i % SMALL_SIZE == SMALL_SIZE - 1 && i < SIZE - 1 )
+        {
+            for ( int j = 0; j < 2 * SIZE + 3; ++j )
                 out << ' ';
             out << std::endl;
         }
@@ -29,133 +36,161 @@ void Sudoku::print(std::ostream& out, bool formatted) const {
 }
 
 
-void Sudoku::read(std::istream& in) {
-    for (int i = 0; i < SIZE; ++i)
-        for (int j = 0; j < SIZE; ++j)
-            in >> matrix[i][j];
+void Sudoku::read( std::istream& in )
+{
+    for ( int i = 0; i < SIZE; ++i )
+        for ( int j = 0; j < SIZE; ++j )
+            in >> matrix[ i ][ j ];
 }
 
 
 bool Sudoku::solve() {
-    simpleSolve();
-    if (!isFilled())
-        return complexSolve();
-    return true;
+    return simpleSolve() && complexSolve();
 }
 
 
-std::ostream& operator<<(std::ostream& out, const Sudoku& sud) {
-    sud.print(out, false);
+std::ostream& operator<<( std::ostream& out, const Sudoku& sud )
+{
+    sud.print( out, false );
     return out;
 }
 
 
-std::istream& operator>>(std::istream& in, Sudoku& sud) {
-    sud.read(in);
+std::istream& operator>>( std::istream& in, Sudoku& sud )
+{
+    sud.read( in );
     return in;
 }
 
 
-bool Sudoku::isFilled() const {
-    for (int i = 0; i < SIZE; ++i)
-        for (int j = 0; j < SIZE; ++j)
-            if (matrix[i][j] == '0')
+bool Sudoku::isFilled() const
+{
+    for ( int i = 0; i < SIZE; ++i )
+        for ( int j = 0; j < SIZE; ++j )
+            if ( matrix[ i ][ j ] == '0' )
                 return false;
+
     return true;
 }
 
 
-bool Sudoku::simpleSolve() {
+bool Sudoku::simpleSolve()
+{
     return removeGuesses();
 }
 
 
-bool Sudoku::complexSolve() {
+bool Sudoku::complexSolve()
+{
     return btSolve();
 }
 
 
 bool Sudoku::btSolve() {
-    if (isFilled())
+    if ( isFilled() )
         return true;
-    Sudoku old(*this);
-    for (int i = 0; i < SIZE; ++i) {
-        for (int j = 0; j < SIZE; ++j) {
-            if (matrix[i][j] == '0') {
-                for (elem guess : old.guesses[i][j]) {
-                    matrix[i][j] = guess;
-                    if (removeGuesses() && btSolve())
+
+    Sudoku old( *this );
+    for ( int i = 0; i < SIZE; ++i )
+    {
+        for ( int j = 0; j < SIZE; ++j )
+        {
+            if ( matrix[ i ][ j ] == '0' ) {
+                for ( const elem& guess : old.guesses[ i ][ j ] )
+                {
+                    matrix[ i ][ j ] = guess;
+                    if ( this->solve() )
                         return true;
                     else
                         *this = old;
                 }
-                if (matrix[i][j] == '0')
+                if ( matrix[ i ][ j ] == '0' )
                     return false;
             }
         }
     }
-    if (isFilled())
-        return true;
-    return false;
+
+    return isFilled();
 }
 
 
 bool Sudoku::removeGuesses() {
     int counter = 0;
-    for (int i = 0; i < SIZE; ++i) {
-        for (int j = 0; j < SIZE; ++j) {
-            if (matrix[i][j] != '0') {
-                guesses[i][j].clear();
+    for ( int i = 0; i < SIZE; ++i )
+    {
+        for ( int j = 0; j < SIZE; ++j )
+        {
+            if ( matrix[ i ][ j ] != '0' )
+            {
+                guesses[ i ][ j ].clear();
                 int blockI;
                 int blockJ;
-                for (int k = 0; k < SIZE; ++k) {
-                    blockI = (i / SMALL_SIZE) * SMALL_SIZE + k / SMALL_SIZE;
-                    blockJ = (j / SMALL_SIZE) * SMALL_SIZE + k % SMALL_SIZE;
-                    counter += guesses[k][j].erase(matrix[i][j]);
-                    counter += guesses[i][k].erase(matrix[i][j]);
-                    counter += guesses[blockI][blockJ].erase(matrix[i][j]);
-                    if ((matrix[k][j] == '0' && guesses[k][j].size() == 0) || (matrix[i][k] == '0' && guesses[i][k].size() == 0) || (matrix[blockI][blockJ] == '0' && guesses[blockI][blockJ].size() == 0))
-                        return false;
+                for ( int k = 0; k < SIZE; ++k )
+                {
+                    blockI      = ( i / SMALL_SIZE ) * SMALL_SIZE + k / SMALL_SIZE;
+                    blockJ      = ( j / SMALL_SIZE ) * SMALL_SIZE + k % SMALL_SIZE;
+                    counter    += guesses[ k ][ j ].erase( matrix[ i ][ j ] );
+                    counter    += guesses[ i ][ k ].erase( matrix[ i ][ j ] );
+                    counter    += guesses[ blockI ][ blockJ ].erase( matrix[ i ][ j ] );
                 }
             }
         }
     }
-    if (counter > 0)
+
+    if ( counter > 0 )
         return setSingleGuesses();
+
     return true;
 }
 
 
-bool Sudoku::setSingleGuesses() {
+bool Sudoku::setSingleGuesses()
+{
     int counter = 0;
-    for (int i = 0; i < SIZE; ++i) {
-        for (int j = 0; j < SIZE; ++j) {
-            if (guesses[i][j].size() == 1) {
-                char guess = *guesses[i][j].begin();
-                if (canBePlacedAt(guess, i, j)) {
+    for ( int i = 0; i < SIZE; ++i )
+    {
+        for ( int j = 0; j < SIZE; ++j )
+        {
+            if ( guesses[ i ][ j ].size() == 1 )
+            {
+                char guess = *guesses[ i ][ j ].begin();
+                if ( canBePlacedAt( guess, i, j ) )
+                {
                     ++counter;
-                    matrix[i][j] = *guesses[i][j].begin();
-                    guesses[i][j].clear();
-                } else {
+                    matrix[ i ][ j ] = *guesses[ i ][ j ].begin();
+                    guesses[ i ][ j ].clear();
+                }
+                else
+                {
                     return false;
                 }
             }
         }
     }
-    if (counter > 0)
+
+    if ( counter > 0 )
         return removeGuesses();
+
     return true;
 }
 
-bool Sudoku::canBePlacedAt(char guess, int i, int j) const {
-    if (matrix[i][j] == '0') {
-        int blockI = (i / SMALL_SIZE) * SMALL_SIZE;
-        int blockJ = (j / SMALL_SIZE) * SMALL_SIZE;
-        for (int k = 0; k < SIZE; ++k) {
-            if (matrix[i][k] == guess || matrix[k][j] == guess || matrix[blockI + k / SMALL_SIZE][blockJ + k % SMALL_SIZE] == guess)
+bool Sudoku::canBePlacedAt( char guess, int i, int j ) const
+{
+    if ( matrix[ i ][ j ] == '0' )
+    {
+        int blockI = ( i / SMALL_SIZE ) * SMALL_SIZE;
+        int blockJ = ( j / SMALL_SIZE ) * SMALL_SIZE;
+        for ( int k = 0; k < SIZE; ++k )
+        {
+            char ik = matrix[ i ][ k ];
+            char kj = matrix[ k ][ j ];
+            char bk = matrix[ blockI + k / SMALL_SIZE ][ blockJ + k % SMALL_SIZE ];
+            if ( ik == guess || kj == guess || bk == guess )
                 return false;
         }
+
         return true;
     }
+
     return false;
 }
